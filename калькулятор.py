895902fc8a65db6cf6685f4e5448335bb6ce425e
Alina-2109 <74093317+Alina-2109@users.noreply.github.com>
2020-11-28 00:38:19 +0300

    Add files via upload

---
+++ калькулятор.py
@@ -0,0 +1,15 @@
+print("Hi")
+f_nam = float(input("Введите первое число")) #первое число
+oper = imput("Введите операцию") #математическое действие
+s_nam = float(input("Введите второе число")) #второе число
+if oper == "+": #сложение
+    print(f_nam + s_nam)
+elif oper == "-": #вычbтание
+    print(f_nam - s_nam)
+elif oper == "*": #умножение
+    print(f_nam * s_nam)
+elif oper == " / ": #деление
+    print(f_nam / s_nam)
+else:
+    Print("Error") #ошибка
+input()
